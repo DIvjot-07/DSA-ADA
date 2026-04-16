611ee39d6cdfae049f98aa04f8fbfb67d7def2e0
Divjot bedi <divjotbedi16706@gmail.com>
2026-04-16 23:56:10 +0530

    Add files via upload

--- lcs.c
+++ lcs.c
@@ -1,10 +1,9 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include <string.h>
 #define M 10
 
-char X[] = "AABC";
-char Y[] = "ADABACZ";
+char X[] = "ABC";
+char Y[] = "XYZ";
 
 int max(int a,int b){
     return (a>b)?a:b;
@@ -19,15 +18,15 @@ int lcs(int i,int j){
     else{
         if(X[i-1]==Y[j-1]){
             if(table[i-1][j-1]!=-1){
-                return (1+ table[i-1][j-1]);
+                return (1 + table[i-1][j-1]);
             }
             else{
                 table[i-1][j-1]=lcs(i-1,j-1);
-                return (1+table[i-1][j-1]);
+                return (1 + table[i-1][j-1]);
             }
         }
         else{
-            if(table[i-1][j]!=-1&& table[i][j-1]!=-1){
+            if(table[i-1][j]!=-1 && table[i][j-1]!=-1){
                 return max(table[i-1][j],table[i][j-1]);
             }
             if(table[i-1][j]==-1){
@@ -36,32 +35,19 @@ int lcs(int i,int j){
             if(table[i][j-1]==-1){
                 table[i][j-1]=lcs(i,j-1);
             }
-            return max(table[i-1][j],    table[i][j-1]);
+            return max(table[i-1][j], table[i][j-1]);
         }
     }
-}
-
-void printLCS(int i, int j) {
-    if (i == 0 || j == 0)
-        return;
-    if (X[i-1] == Y[j-1]) {
-        printLCS(i-1, j-1);
-        printf("%c", X[i-1]);
-    }
-    else if (table[i-1][j] > table[i][j-1])
-        printLCS(i-1, j);
-    else
-        printLCS(i, j-1);
 }
 
 int main(){
     int i=strlen(X);
     int j=strlen(Y);
-    for (int k=0;k< M;k++)
-        for (int l= 0;l< M;l++)
+
+    for (int k=0;k<M;k++)
+        for (int l=0;l<M;l++)
             table[k][l] = -1;
-    printf("Length of LCS is %d\n",lcs(i,j));
-    printf("LCS = ");
-    printLCS(i,j);
+
+    printf("Length of LCS is %d", lcs(i,j));
     return 0;
 }
